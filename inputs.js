e9aecb352562c8746752f08cad537a9b3ecd8e8f
Brendan Elton <brendan.elton@gmail.com>
2021-12-07 19:18:21 +1100

    first commit

---
+++ inputs.js
@@ -0,0 +1,4 @@
+// INPUTS
+const NftCollectionTitle = 'the-fungible-by-pak' //Title of NFT collection
+const printTimer = 5 //Minutes between bot posting floor price
+export { NftCollectionTitle, printTimer }
